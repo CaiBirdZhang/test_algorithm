3f57d7a64515a6d41493a816893cd37923c57bfd
CaiBirdZhang <412546262@qq.com>
2018-03-29 13:59:19 +0800

    add dijkstra function

--- src/graph/dijkstra/ListUDG.cpp
+++ src/graph/dijkstra/ListUDG.cpp
@@ -116,7 +116,51 @@ int ListUDG::getWeight(int start, int end)
  */
 void ListUDG::dijkstra(int vs, int prev[], int dist[])
 {
-
+    // 每个顶点是否已经找到最短
+    int* flag = new int[mVexNum] ();
+    // 初始化权值
+    for (int i = 0; i < mVexNum; ++i)
+    {
+        dist[i] = getWeight(vs, i);
+    }
+    // 起始点
+    flag[vs] = 1;
+    dist[vs] = 0;
+    // 所有顶点
+    int min = INF;
+    int tmpIndex;
+    for (int i = 1; i < mVexNum; ++i)
+    {
+        min = INF;
+        // 每次获取T(未排序)中最小的距离
+        for (int j = 0; j < mVexNum; ++j)
+        {
+            if (flag[j] == 0 && dist[j] < min)
+            {
+                min = dist[j];
+                tmpIndex = j;
+            }
+        }
+        // 标记"顶点tmpIndex"为已经获取到最短路径
+        flag[tmpIndex] = 1;
+        //                                     min                getWeight
+        // 更新最短路径信息，dis(vs, j) > dis(vs, tmpIndex) + dis(tmpIndex, j)
+        for (int j = 0; j < mVexNum; ++j)
+        {
+            int tmp = getWeight(tmpIndex, j);
+            tmp = (tmp == INF ? INF : (min + tmp)); // 防止溢出
+            if (flag[j] == 0 && dist[j] > tmp)
+            {
+                dist[j] = tmp;
+            }
+        }
+    }
+    // 打印信息
+    cout << "vertex(" << mVexs[vs].data << ") to other vertexs distance:" << endl;
+    for (int i = 0; i < mVexNum; ++i)
+    {
+        cout << mVexs[vs].data << " => " << mVexs[i].data << " (" << dist[i] << ")" << endl;
+    }
 }
 
 
